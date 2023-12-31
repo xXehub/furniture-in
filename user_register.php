a538b68669608c9230440039567ff86fc3c060af
moonalisa1337 <102500941+xXehub@users.noreply.github.com>
2024-01-01 01:12:15 +0700

    code update #2
    ui changes

--- user_register.php
+++ user_register.php
@@ -1,17 +1,12 @@
 <?php
-
 include 'components/connect.php';
-
 session_start();
-
 if (isset($_SESSION['user_id'])) {
    $user_id = $_SESSION['user_id'];
 } else {
    $user_id = '';
 };
-
 if (isset($_POST['submit'])) {
-
    $name = $_POST['name'];
    $name = filter_var($name, FILTER_SANITIZE_STRING);
    $email = $_POST['email'];
@@ -37,9 +32,7 @@ if (isset($_POST['submit'])) {
       }
    }
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -52,45 +45,39 @@ if (isset($_POST['submit'])) {
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css?=<?php echo time(); ?>"">
+   <link rel="stylesheet" href="./assets/css/style_sakkarepmu.css">
+
+   <!--- google font link-->
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
 </head>
 <body>
-   
-<?php include 'components/user_header.php'; ?>
+   <?php include 'components/user_header.php'; ?>
+   <div class="login">
+      <div class="container">
+         <div class="slider-item">
+            <!-- <img src="./assets/images/Quirky-house-Banner.jpg" alt="women's latest fashion sale" class="banner-img"> -->
+            <div class="login-content">
+               <section class=" form-container">
 
-<section class=" form-container">
-
-   <form action="" method="post">
-      <h3>buat akun</h3>
-      <input type="text" name="name" required placeholder="masukkan username" maxlength="20" class="box">
-      <input type="email" name="email" required placeholder="masukkan email" maxlength="50" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="password" name="pass" required placeholder="mmasukkan password" maxlength="20" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="password" name="cpass" required placeholder="konfirmasi password" maxlength="20" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="submit" value="register now" class="btn" name="submit">
-      <p>sudah punya akun?</p>
-      <a href="user_login.php" class="option-btn">login sekarang</a>
-   </form>
-
-   </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
+                  <form action="" method="post">
+                     <!-- <h3>buat akun</h3> -->
+                     <input type="text" name="name" required placeholder="masukkan username" maxlength="20" class="kolom-field">
+                     <input type="email" name="email" required placeholder="masukkan email" maxlength="50" class="kolom-field" oninput="this.value = this.value.replace(/\s/g, '')">
+                     <input type="password" name="pass" required placeholder="mmasukkan password" maxlength="20" class="kolom-field" oninput="this.value = this.value.replace(/\s/g, '')">
+                     <input type="password" name="cpass" required placeholder="konfirmasi password" maxlength="20" class="kolom-field" oninput="this.value = this.value.replace(/\s/g, '')">
+                     <input type="submit" value="daftar sekarang" class="btn-login" name="submit">
+                     <p>sudah punya akun?</p>
+                     <a class="login-subtittle" href="user_login.php" class="option-btn">login sekarang</a>
+                  </form>
+               </section>
+            </div>
+         </div>
+      </div>
+   </div>
    <?php include 'components/footer.php'; ?>
-
    <script src="js/script.js"></script>
-
-   </body>
-
+</body>
 </html>

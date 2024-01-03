9ac950c3641c547987d07e2f962899b7ad7372a3
moonalisa1337 <102500941+xXehub@users.noreply.github.com>
2024-01-04 00:53:12 +0700

    merapikan ui dan fix bug

--- wishlist.php
+++ wishlist.php
@@ -40,7 +40,7 @@ if (isset($_GET['delete_all'])) {
    <link rel="shortcut icon" href="./assets/images/logo/favicon.ico" type="image/x-icon">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
    <!--- css link lur, eksternal btw-->
-   <link rel="stylesheet" href="./css/style_sakkarepmu.css">
+   <link rel="stylesheet" href="./assets/css/style_sakkarepmu.css">
 
    <!--- google font link-->
    <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -53,25 +53,13 @@ if (isset($_GET['delete_all'])) {
 
    <?php include 'components/user_header.php'; ?>
    <div class="product-container">
-
       <div class="container">
-
-
-
-         <div class="product-box">
-            <!-- <section class="products"> -->
-            <div class="product-main">
-               <h2 class="title">New produk_sakkarepmu</h2>
-               <div class="product-grid">
-                  <div class="product-grid">
-                  <?php
-                     $select_products = $conn->prepare("SELECT * FROM `products` LIMIT 6");
-                     $select_products->execute();
-                     if ($select_products->rowCount() > 0) {
-                        while ($fetch_product = $select_products->fetch(PDO::FETCH_ASSOC)) {
-                           
-                     ?>
-                     <!-- query buat select whishlist -->
+         <!-- total barang -->
+         <div class="sidebar  has-scrollbar" data-mobile-menu>
+            <div class="product-showcase">
+               <h3 class="showcase-heading">TOTAL BARANG</h3>
+               <div class="showcase-wrapper">
+                  <div class="showcase-container">
                      <?php
                      $grand_total = 0;
                      $select_wishlist = $conn->prepare("SELECT * FROM `wishlist` WHERE user_id = ?");
@@ -80,106 +68,90 @@ if (isset($_GET['delete_all'])) {
                         while ($fetch_wishlist = $select_wishlist->fetch(PDO::FETCH_ASSOC)) {
                            $grand_total += $fetch_wishlist['price'];
                      ?>
-                           <form action="" method="post" class="swiper-slide slide">
-                              <div class="showcase">
-                                 <div class="showcase-banner">
-                                    <input type="hidden" name="pid" value="<?= $fetch_wishlist['pid']; ?>">
-                                    <input type="hidden" name="wishlist_id" value="<?= $fetch_wishlist['id']; ?>">
-                                    <input type="hidden" name="name" value="<?= $fetch_wishlist['name']; ?>">
-                                    <input type="hidden" name="price" value="<?= $fetch_wishlist['price']; ?>">
-                                    <input type="hidden" name="image" value="<?= $fetch_wishlist['image']; ?>">
 
-                                    <img src="uploaded_img/<?= $fetch_wishlist['image']; ?>" alt="wishlist" width="300" class="product-img default">
-
-
-                                    <div class="showcase-content">
-                                       <a href="#" class="showcase-category"><?= $fetch_product['kategori']; ?></a>
-                                       <a href="quick_view.php?pid=<?= $fetch_product['id']; ?>">
-                                          <h3 class="showcase-title"><?= $fetch_product['name']; ?></h3>
-                                          <div class="price-box">
-                                             <div class="price">Rp. <?= $fetch_wishlist['price']; ?>,00</div>
-                                             <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
-                                          </div>
-                                          <input type="submit" value="tambahkan ke keranjang" class="btn-newsletter" name="add_to_cart">
-                                          <input type="submit" value="hapus produk" onclick="return confirm('delete this from wishlist?');" class="btn-newsletter" name="delete">
-                                    </div>
-
-
-
-                                    <p class="showcase-badge">Wishlist</p>
-                                    <div href="quick_view.php?pid=<?= $fetch_product['id']; ?>" class="showcase-actions">
-                                       <button class="btn-action" type="submit" name="add_to_wishlist">
-                                          <ion-icon name="heart-outline"></ion-icon>
-                                       </button>
-                                       <a href="quick_view.php?pid=<?= $fetch_product['id']; ?>" class="btn-action">
-                                          <ion-icon href="quick_view.php?pid=<?= $fetch_product['id']; ?>" name="eye-outline"></ion-icon>
-                                       </a>
-                                       <!-- <button class="btn-action">
-                                       <ion-icon name="repeat-outline"></ion-icon>
-                                    </button>
-                                    <button class="btn-action">
-                                       <ion-icon name="bag-add-outline"></ion-icon>
-                                    </button> -->
-                                    </div>
-                                 </div>
-
-                              </div>
-                              
-                           </form>
                      <?php
                         }
                      } else {
                         echo '<p class="empty">no products added yet!</p>';
                      }
                      ?>
+                     <div class="showcase">
+                        <div class="showcase-content">
+                           <div class="product-showcase">
+                              <p>total semua : <span>Rp.<?= $grand_total; ?>,00</span></p>
+                              <a href="home.php" class="btn btn-wishlist">lanjut belanja</a>
+                              <a href="wishlist.php?delete_all" class="btn btn-delete <?= ($grand_total > 1) ? '' : 'disabled'; ?>" onclick="return confirm('hapus semua produk dari wishlist?');">hapus semua produk</a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
 
-<?php
-                        }
-                     } else {
-                        echo '<p class="empty">no products added yet!</p>';
+               </div>
+            </div>
+         </div>
+
+         <div class="product-box">
+            <!-- <section class="products"> -->
+            <div class="product-main">
+               <h2 class="title">WISHLIST PRODUK</h2>
+               <div class="product-grid">
+                  <!-- query buat select whishlist -->
+                  <?php
+                  $grand_total = 0;
+                  $select_wishlist = $conn->prepare("SELECT * FROM `wishlist` WHERE user_id = ?");
+                  $select_wishlist->execute([$user_id]);
+                  if ($select_wishlist->rowCount() > 0) {
+                     while ($fetch_wishlist = $select_wishlist->fetch(PDO::FETCH_ASSOC)) {
+                        $grand_total += $fetch_wishlist['price'];
+                  ?>
+                        <form action="" method="post" class="swiper-slide slide">
+                           <div class="showcase">
+                              <div class="showcase-banner">
+                                 <input type="hidden" name="pid" value="<?= $fetch_wishlist['pid']; ?>">
+                                 <input type="hidden" name="wishlist_id" value="<?= $fetch_wishlist['id']; ?>">
+                                 <input type="hidden" name="name" value="<?= $fetch_wishlist['name']; ?>">
+                                 <input type="hidden" name="price" value="<?= $fetch_wishlist['price']; ?>">
+                                 <input type="hidden" name="image" value="<?= $fetch_wishlist['image']; ?>">
+                                 <img src="uploaded_img/<?= $fetch_wishlist['image']; ?>" alt="wishlist" width="300" class="product-img default">
+                                 <div class="showcase-content">
+                                    <div class="name"><?= $fetch_wishlist['name']; ?></div>
+                                    <div class="price-box">
+                                    <div class="flex">
+                                    <div class="price">Rp. <?= $fetch_wishlist['price']; ?>,00</div>
+                                    <input type="number" name="qty" class="showcase" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
+                                      <!-- <button type="submit" class="" ><a class="fas fa-edit" name="update_qty"> </a></button> -->
+                                    </div>
+                                    </div>
+                                    <input type="submit" value="keranjang" class="btn btn-wishlist" name="add_to_cart">
+                                    <input type="submit" value="hapus" onclick="return confirm('hapus dari wishlist?');" class="btn-wishlist" name="delete">
+
+                                 </div>
+                                 <p class="showcase-badge">Wishlist</p>
+
+                                 <!-- <button class="btn-action">
+                                       <ion-icon name="repeat-outline"></ion-icon>
+                                    </button>
+                                    <button class="btn-action">
+                                       <ion-icon name="bag-add-outline"></ion-icon>
+                                    </button> -->
+                              </div>
+                           </div>
+                        </form>
+                  <?php
                      }
-                     ?>
-
-
-                  </div>
-                  <!-- <h3 class="heading"> wishlist anda</h3>
-
-            <div class="box-container"> -->
-
-
-
-                  <!-- </div> -->
-
-                  <div class="wishlist-total">
-                     <p>total semua : <span>Rp.<?= $grand_total; ?>,00</span></p>
-                     <a href="shop.php" class="option-btn">lanjut belanja</a>
-                     <a href="wishlist.php?delete_all" class="delete-btn <?= ($grand_total > 1) ? '' : 'disabled'; ?>" onclick="return confirm('hapus semua produk dari wishlist?');">hapus semua produk</a>
-
-                  </div>
+                  } else {
+                     echo '<p class="empty">no products added yet!</p>';
+                  }
+                  ?>
                </div>
             </div>
          </div>
       </div>
    </div>
-
+   </div>
+   </div>
    <!-- </section> -->
-
-
-
-
-
-
-
-
-
-
-
-
-
    <?php include 'components/footer.php'; ?>
-
    <script src="js/script.js"></script>
-
 </body>
-
 </html>
